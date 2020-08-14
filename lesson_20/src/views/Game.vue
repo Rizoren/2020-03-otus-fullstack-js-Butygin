81b284790440749f1548b44289e631b198f61309
Rizoren <rizoren@yandex.ru>
2020-08-14 21:10:40 +0500

    Lesson 23 - Store for Settings

--- lesson_20/src/views/Game.vue
+++ lesson_20/src/views/Game.vue
@@ -7,51 +7,39 @@
         </v-btn>
       </v-col>
       <v-col class="ma-0 pa-1" cols="12" sm="2">
-        <v-text-field class="centered-input"
-                      outlined label="Time" type="time" value="5:00" readonly></v-text-field>
+        <v-text-field class="centered-input" :value="date.format('mm:ss')"
+                      outlined label="Time" readonly>
+        </v-text-field>
       </v-col>
     </v-row>
-    <v-row align="center" justify="center">
-      <v-col class="ma-0 pa-1" cols="12" sm="2">
+
+    <v-row class="align-baseline" justify="center" v-if="!endGame">
+      <v-col :key="`col${idx}`" v-for="(elem, idx) in expression" class="ma-0 pa-1" cols="12"
+             :sm="(idx + 1) % 2 === 0 ? 1 : 2">
         <v-text-field class="centered-input"
-                      outlined label="1st" type="number" value="123"></v-text-field>
-      </v-col>
-      <v-col class="ma-0 pa-1" cols="12" sm="1">
-        <v-text-field class="centered-input"
-                      outlined disabled
-                      value="*"></v-text-field>
-      </v-col>
-      <v-col class="ma-0 pa-1" cols="12" sm="2">
-        <v-text-field class="centered-input"
-                      outlined label="2nd" type="number" value="123"></v-text-field>
-      </v-col>
-      <v-col class="ma-0 pa-1" cols="12" sm="1">
-        <v-text-field class="centered-input"
-                      outlined disabled
-                      value="/"></v-text-field>
-      </v-col>
-      <v-col class="ma-0 pa-1" cols="12" sm="2">
-        <v-text-field class="centered-input"
-                      outlined label="3rd" type="number" value="123"></v-text-field>
-      </v-col>
-      <v-col class="ma-0 pa-1" cols="12" sm="1">
-        <v-text-field class="centered-input"
-                      outlined disabled
-                      value="="></v-text-field>
-      </v-col>
-      <v-col class="ma-0 pa-1" cols="12" sm="2">
-        <v-text-field class="centered-input"
-                      outlined label="Result" type="number" value="123"></v-text-field>
+                      outlined :label="(idx + 1) % 2 !== 0 ? `${idx + 1}` : ''"
+                      :type="(idx + 1) % 2 === 0 ? 'text' : 'number'"
+                      :readonly="elem.el.length !== 0"
+                      @focus="setInputFocus($event)"
+                      :autofocus="elem.el.length === 0"
+                      :placeholder="elem.el.length === 0 ? 'unknown' : ''"
+                      :value="elem.el"></v-text-field>
       </v-col>
     </v-row>
+    <v-row class="align-baseline" justify="center" v-else>
+      <v-alert class="pa-5 ma-1" outlined text
+               transition="scale-transition" type="warning">Game over...</v-alert>
+    </v-row>
+
     <v-row align="center" justify="center">
-      <v-btn class="ma-1" tile icon outlined color="success" width="10%" height="5vh">
-        <v-icon>mdi-chevron-left</v-icon>
-      </v-btn>
-      <v-btn class="ma-1" tile outlined color="warning" width="30%" height="5vh">Send</v-btn>
-      <v-btn class="ma-1" tile icon outlined color="success" width="10%" height="5vh">
-        <v-icon>mdi-chevron-right</v-icon>
-      </v-btn>
+<!--      <v-btn class="ma-1" tile icon outlined color="success" width="10%" height="5vh">-->
+<!--        <v-icon>mdi-chevron-left</v-icon>-->
+<!--      </v-btn>-->
+      <v-btn class="ma-1" tile outlined color="warning" width="30%" height="5vh"
+             @click="setNext">Send</v-btn>
+<!--      <v-btn class="ma-1" tile icon outlined color="success" width="10%" height="5vh">-->
+<!--        <v-icon>mdi-chevron-right</v-icon>-->
+<!--      </v-btn>-->
     </v-row>
     <v-row align="center" justify="center">
       <v-btn class="ma-1" tile outlined color="success" width="5vh" height="5vh">7</v-btn>
@@ -77,8 +65,69 @@
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
   name: 'Game',
+  data() {
+    return {
+      hasBeenFocusedInput: null,
+      settings: this.$store.state.settings,
+      date: moment(this.$store.state.settings.duration * 60 * 1000),
+      interval: null,
+      endGame: false,
+      examples: this.$store.getters.getExamplesSelf,
+      curExample: null,
+      curIndex: 0,
+    };
+  },
+  methods: {
+    setInputFocus(event) {
+      this.hasBeenFocusedInput = event.target;
+    },
+    setNext() {
+      this.curIndex += 1;
+      if (this.curIndex < this.examples.length) {
+        this.curExample = this.examples[this.curIndex];
+      } else {
+        this.endGame = true;
+      }
+    },
+  },
+  computed: {
+    expression: () => {
+      const exprToParse = '1+1=2'; // this.curExample.example
+      let num = Math.floor(Math.random() * (exprToParse.length - 1 + 1)) + 1;
+      const half = Math.random() > 0.5 ? num - 1 : num + 1;
+      num = num % 2 === 0 ? half : num;
+      const expr = [];
+      exprToParse.split('').forEach((el, ind) => {
+        let type = 'number';
+        if ('+*/-^='.indexOf(el.trim()) >= 0) {
+          type = 'operator';
+        } else if (ind === num - 1) {
+          type = 'unknown';
+        }
+        expr.push({ el: type !== 'unknown' ? el : '', type });
+      });
+      return expr;
+    },
+  },
+  mounted() {
+    this.curExample = this.examples[this.curIndex];
+    this.interval = setInterval(() => {
+      if (!this.endGame) {
+        this.date = moment(this.date.subtract(100, 'millisecond'));
+        this.curExample = this.examples[this.curIndex];
+      }
+      if (this.date.format('mm:ss') === '00:00') {
+        this.endGame = true;
+      }
+    }, 100);
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
 };
 </script>
 
